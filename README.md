cc8aeb1576c13138d5c14b94373a5bf4519f94cb
Nick Bruun <nick@bruun.co>
2015-11-05 14:29:08 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,73 @@
+# Broccoli Zopfliy gzip plugin
+
+Fork of [broccoli-gzip](https://github.com/salsify/broccoli-gzip) to use Zopfli instead of gzip to perform gzip compression. All credit goes to the original authors of broccoli-gzip.
+
+
+## Installation
+
+```bash
+$ npm install broccoli-zopfli
+```
+
+
+## Example
+
+```javascript
+var zopfliGzipFiles = require('broccoli-zopfli');
+
+var tree = zopfliGzipFiles('app', {
+  extensions: ['js', 'css', 'svg']
+});
+```
+
+
+## Configuration
+
+### `zopfliGzipFiles(inputTree, options)`
+
+---
+
+`options.extensions` *{Array}* (required)
+
+The file extensions that should be compressed.
+
+---
+
+`options.keepUncompressed` *{Boolean}* (optional, default `false`)
+
+Whether to keep uncompressed versions of the files in the resulting tree.
+
+---
+
+`options.appendSuffix` *{Boolean}* (optional, default `true`)
+
+Whether to append the `.gz` extension suffix to compressed files.
+
+---
+
+`options.numIterations` *{Number}* (optional, default `15`)
+
+Maximum amount of times to rerun forward and backward pass to optimize LZ77 compression cost. Good values: 10, 15 for small files, 5 for files over several MB in size or it will be too slow.
+
+---
+
+`options.blockSplitting` *{Boolean}* (optional, default `true`) 
+
+If true, splits the data in multiple deflate blocks with optimal choice for the block boundaries. Block splitting gives better compression.
+
+---
+
+`options.blockSplittingLast` *{Boolean}* (optional, default `false`) 
+
+If true, chooses the optimal block split points only after doing the iterative LZ77 compression. If false, chooses the block split points first, then does iterative LZ77 on each individual block. Depending on the file, either first or last gives the best compression.
+
+---
+
+`options.blockSplittingMax` *{Number}* (optional, default `15`) 
+
+Maximum amount of blocks to split into (0 for unlimited, but this can give extreme results that hurt compression on some files).
+
+
+## License
+
+broccoli-zopfli is, like broccoli-gzip, distributed under the MIT license.
---
+++ index.js
@@ -0,0 +1,68 @@
+/*global require: true, module: true*/
+var RSVP = require('rsvp'),
+    helpers = require('broccoli-kitchen-sink-helpers'),
+    Filter = require('broccoli-filter'),
+    Buffer = require('buffer').Buffer,
+    zopfli = require('node-zopfli');
+
+
+ZopfliFilter.prototype = Object.create(Filter.prototype);
+ZopfliFilter.prototype.constructor = ZopfliFilter;
+
+
+/**
+ * Zopfli filter.
+ *
+ * @constructor
+ * @param {object} inputTree - Input tree.
+ * @param {object} options - Options.
+ */
+function ZopfliFilter(inputTree, options) {
+    if (!(this instanceof ZopfliFilter))
+        return new ZopfliFilter(inputTree, options);
+
+    options = options || {};
+
+    this.zopfliOptions = {
+        numiterations: options.numIterations || 15,
+        blocksplitting: (options.blockSplitting === undefined ?
+                         true :
+                         options.blockSplitting),
+        blocksplittinglast: !!options.blockSplittingLast,
+        blocksplittingmax: (options.blockSplittingMax === undefined ?
+                            15 :
+                            options.blockSplittingMax)
+    };
+    this.keepUncompressed = options.keepUncompressed;
+    this.appendSuffix = (options.hasOwnProperty('appendSuffix') ?
+                         options.appendSuffix :
+                         true);
+
+    if (this.keepUncompressed && !this.appendSuffix) {
+        throw new Error('Cannot keep uncompressed files without appending suffix. Filenames would be the same.');
+    }
+
+    Filter.apply(this, arguments);
+}
+
+ZopfliFilter.prototype.processFile = function(srcDir, destDir, relativePath) {
+    if (this.keepUncompressed) {
+        helpers.copyPreserveSync(srcDir + '/' + relativePath,
+                                 destDir + '/' + relativePath);
+    }
+
+    return Filter.prototype.processFile.apply(this, arguments);
+};
+
+ZopfliFilter.prototype.processString = function(str) {
+    return RSVP.denodeify(zopfli.gzip)(new Buffer(str), this.zopfliOptions);
+};
+
+ZopfliFilter.prototype.getDestFilePath = function() {
+    var destFilePath = Filter.prototype.getDestFilePath.apply(this, arguments);
+    if (destFilePath) {
+        return this.appendSuffix ? destFilePath + '.gz' : destFilePath;
+    }
+};
+
+module.exports = ZopfliFilter;
